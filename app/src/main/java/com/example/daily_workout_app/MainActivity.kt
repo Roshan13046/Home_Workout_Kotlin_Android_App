0899e347294a3a9b99174d4c38c67f2f2b8a2e87
Roshan Prakash Raut <roshanpk.raut@gmail.com>
2020-11-05 10:52:00 +0530

    Home UI Created with start button.

---
+++ app/src/main/java/com/example/daily_workout_app/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.daily_workout_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        llStart.setOnClickListener { Toast.makeText(this, "We Will Start the workout Exercise",
+            Toast.LENGTH_SHORT).show() }
+    }
+}
