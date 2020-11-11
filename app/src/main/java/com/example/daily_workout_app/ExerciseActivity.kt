1d221ba8f1cd7046019fbc72763123552da4cfd6
Roshan Prakash Raut <roshanpk.raut@gmail.com>
2020-11-11 08:09:18 +0530

    Layer Drawable Button Timer added in Exercise screen.

--- app/src/main/java/com/example/daily_workout_app/ExerciseActivity.kt
+++ app/src/main/java/com/example/daily_workout_app/ExerciseActivity.kt
@@ -2,9 +2,13 @@ package com.example.daily_workout_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_exercise.*
 
 class ExerciseActivity : AppCompatActivity() {
+    private var restTimer: CountDownTimer?=null
+    private var restProgress = 0// progress from 0 to 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +19,47 @@ class ExerciseActivity : AppCompatActivity() {
         if(actionbar != null){
             actionbar.setDisplayHomeAsUpEnabled(true)
         }
-        toolbar_exercise_activity.setNavigationOnClickListener { onBackPressed() }
+        toolbar_exercise_activity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        setupRestView()
     }
+
+    override fun onDestroy(){
+        if(restTimer != null){
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+
+        super.onDestroy()
+    }
+
+    private fun setRestProgressBar(){
+        progressBar.progress = restProgress
+        restTimer = object: CountDownTimer(10000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                progressBar.progress = 10 - restProgress
+                tvTimer.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,
+                    "Now, We will Start Exercise.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.start()
+    }
+
+    private fun setupRestView(){
+        if(restTimer != null){
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
+    }
+
 }
