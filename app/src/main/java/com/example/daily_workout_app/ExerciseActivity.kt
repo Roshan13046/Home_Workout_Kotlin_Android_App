655baf555c7e98eed420dabedf741b5df7744f5e
Roshan Prakash Raut <roshanpk.raut@gmail.com>
2020-12-30 16:13:26 +0530

    Added text to speech feature.

--- app/src/main/java/com/example/daily_workout_app/ExerciseActivity.kt
+++ app/src/main/java/com/example/daily_workout_app/ExerciseActivity.kt
@@ -3,11 +3,15 @@ package com.example.daily_workout_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_exercise.*
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     //variable for rest timer
     private var restTimer: CountDownTimer?=null
     private var restProgress = 0// progress from 0 to 10
@@ -25,6 +29,8 @@ class ExerciseActivity : AppCompatActivity() {
     private var exerciseList: ArrayList<ExerciseModel>?=null
     private var currentExercisePosition = -1
 
+    private var tts: TextToSpeech? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +45,8 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        tts = TextToSpeech(this,this)
+
         exerciseList = Constants.defaultExerciseList()
         setupRestView()
     }
@@ -47,6 +55,16 @@ class ExerciseActivity : AppCompatActivity() {
         if(restTimer != null){
             restTimer!!.cancel()
             restProgress = 0
+        }
+
+        if(exerciseTimer != null){
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
+        }
+
+        if(tts!=null){
+            tts!!.stop()
+            tts!!.shutdown()
         }
 
         super.onDestroy()
@@ -120,10 +138,31 @@ class ExerciseActivity : AppCompatActivity() {
             exerciseTimer!!.cancel()
             exerciseProgress = 0
         }
+
+        speakOut(exerciseList!![currentExercisePosition].getName())
+
         setExerciseProgressBar()
 
         ivImage.setImageResource(exerciseList!![currentExercisePosition].getImage())
         tvExerciseName.text = exerciseList!![currentExercisePosition].getName()
+    }
+
+    override fun onInit(status: Int) {
+        if(status == TextToSpeech.SUCCESS){
+            //setting the language to US English
+            val result = tts!!.setLanguage(Locale.US)
+            if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                Log.e("TTS","Language specified is not supported")
+                Toast.makeText(this,"Language not Supported",Toast.LENGTH_SHORT).show()
+            }else{
+                Log.e("TTS","Initialization Failed!")
+            }
+        }
+    }
+
+    private fun speakOut(text: String){
+        //ignore this error: It working fine after installing the app
+        tts!!.speak(text, TextToSpeech.QUEUE_FLUSH,null,"")
     }
 
 }
