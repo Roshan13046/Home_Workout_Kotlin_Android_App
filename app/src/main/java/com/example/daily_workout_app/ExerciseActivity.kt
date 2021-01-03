6ae9ace65ffb7b090a2146079689f7f04c89ad0f
Roshan Prakash Raut <roshanpk.raut@gmail.com>
2021-01-03 15:05:59 +0530

    custom dialog box added for back button.

--- app/src/main/java/com/example/daily_workout_app/ExerciseActivity.kt
+++ app/src/main/java/com/example/daily_workout_app/ExerciseActivity.kt
@@ -1,5 +1,6 @@
 package com.example.daily_workout_app
 
+import android.app.Dialog
 import android.content.Intent
 import android.media.MediaPlayer
 import android.net.Uri
@@ -12,6 +13,7 @@ import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_exercise.*
+import kotlinx.android.synthetic.main.custom_dialog_for_exit.*
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -53,7 +55,7 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             actionbar.setDisplayHomeAsUpEnabled(true)
         }
         toolbar_exercise_activity.setNavigationOnClickListener {
-            onBackPressed()
+            customDialogForBackButton()
         }
 
         tts = TextToSpeech(this,this)
@@ -215,5 +217,22 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         exerciseAdapter = ExerciseStatusAdapter(exerciseList!!,this)
         rvExerciseStatus.adapter = exerciseAdapter
     }
+
+    private fun customDialogForBackButton(){
+        val customDialog = Dialog(this)
+
+        customDialog.setContentView(R.layout.custom_dialog_for_exit)
+        customDialog.tvYes.setOnClickListener{
+            finish()
+            customDialog.dismiss()
+        }
+
+        customDialog.tvNo.setOnClickListener{
+            customDialog.dismiss()
+        }
+        customDialog.show()
+    }
+
+
 
 }
